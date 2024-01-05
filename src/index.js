7c95ee191119dce3b731d785c4cc184010afb191
zeid <zeidhassan4@gmail.com>
2024-01-05 21:04:45 +0300

    Comlited assignment

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
+  let form = document.querySelector('form')
+  form.addEventListener('sumbit', (e) => {
+    e.preventDefault()
+    buildToDo(e.target ['new-task-description'].value)
+    form.reset()
+  })
+
+  function buildToDo(todo){
+    let p = document.createElement('p')
+    let btn = document.createElement('button')
+    btn.addEventListener('click', handleRemove)
+    btn.textContent='x'
+    p.textContent = `${todo}`
+    p.appendChild(btn)
+    document.querySelector('#list').appendChild(p)
+  }
+
+  function handleRemove(e){
+    e.target.parentNode.remove()
+  }
+  
 });
